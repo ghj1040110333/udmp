a1a1857fb0dfdb69c98e054a7d9ed99f468c39a1
npccsb <npccsb0204@sina.com>
2017-04-28 12:01:35 +0800

    提交test的修改变更

--- udmp-cloud/udmp_springcloud_eureka/src/test/java/cn/com/git/udmp/cloud/eureka/EurekaApplicationTest.java
+++ udmp-cloud/udmp_springcloud_eureka/src/test/java/cn/com/git/udmp/cloud/eureka/EurekaApplicationTest.java
@@ -4,17 +4,25 @@ package cn.com.git.udmp.cloud.eureka;
 
 import static org.junit.Assert.assertEquals;
 
+import java.net.URI;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Eureka Runtime Server的单元测试类 
@@ -25,18 +33,34 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = EurekaApplication.class)
 @WebAppConfiguration
+@RestController
+@EnableEurekaClient
 public class EurekaApplicationTest {
     
     private int port = 8761;
-
+    
+    @Autowired
+    DiscoveryClient client;
+    
     @Test
+    public void echo(){
+        List<ServiceInstance> list = client.getInstances("UDMP-EUREKA-CLIENT");
+        if (list != null && list.size() > 0 ) {
+            URI uri = list.get(0).getUri();
+            if (uri !=null ) {
+                System.out.println(":::::::::::: "+new RestTemplate().getForObject(uri,String.class));
+            }
+        }
+    }
+
+//    @Test
     public void catalogLoads() {
         @SuppressWarnings("rawtypes")
         ResponseEntity<Map> entity = new TestRestTemplate().getForEntity("http://localhost:" + port + "/eureka/apps", Map.class);
         assertEquals(HttpStatus.OK, entity.getStatusCode());
     }
 
-    @Test
+//    @Test
     public void adminLoads() {
         @SuppressWarnings("rawtypes")
         ResponseEntity<Map> entity = new TestRestTemplate().getForEntity("http://localhost:" + port + "/env", Map.class);
